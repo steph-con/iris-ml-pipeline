cd4f7389271733ad749b6d6b65772715b83aff5d
stephcon <stephcon@live.com>
2025-08-29 22:03:15 +0100

    Algorithm evaluation and comparison

--- iris_analysis.py
+++ iris_analysis.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-
 from sklearn import model_selection, metrics
+# from dataclasses import dataclass
+
+# import models
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -19,13 +21,13 @@ from sklearn.datasets import load_iris
 iris = load_iris()
 
 # map target names to target and clasify the different iris
-names = iris.target_names[iris.target]
+target_names = iris.target_names[iris.target]
 features = iris.feature_names
 
 
 # Prepare dataframe
 df_iris = pd.DataFrame(data = iris.data, columns=features)
-df_iris["species"] = names
+df_iris["species"] = target_names
 
 # %%
 print("Check general dataset information")
@@ -40,7 +42,11 @@ print("-"*35)
 print(df_iris.groupby("species").size())
 
 
+##############################################################
 # %%
+# Data Visualisation
+##############################################################
+
 # Plot the different attributes of the dataset to check distributions
 fig1 = plt.figure(figsize=(7,5))
 
@@ -52,7 +58,6 @@ sns.violinplot(
 plt.tight_layout()
 plt.show()
 
-##############################################################
 # %%
 # Plot histograms for the different attributes
 df_iris.hist(figsize=(6,6))
@@ -60,6 +65,8 @@ df_iris.hist(figsize=(6,6))
 plt.tight_layout()
 plt.show()
 
+
+# FROM SOURCE
 print("It looks like perhaps two of the input variables have a Gaussian\n" \
     "distribution. This is useful to note as we can use algorithms\n" \
     "that can exploit this assumption.")
@@ -81,7 +88,6 @@ print("It looks like the features of all species display a Gausian distribution.
 
 plt.show()
 
-############################################################
 # %%
 # Multivariate plots
 print("Check interactions between the variables. Plot attribute pairs.")
@@ -93,9 +99,81 @@ plt.show()
 
 
 
+##############################################################
 # %%
+# Algorithm evaluation
+##############################################################
+
+X = df_iris[features].values
+y = df_iris["species"].values
+
+train_X, val_X, train_y, val_y = model_selection.train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=1
+)
+
+# FROM SOURCE
+# Use stratified 10-fold cross validation to estimate model accuracy.
+# This will split our dataset into 10 parts, train on 9 and test on 1 and
+# repeat for all combinations of train-test splits.
+
+##############################################################
+## Test 6 different algorithms
+# Logistic Regression (LR)
+# Linear Discriminant Analysis (LDA)
+# K-Nearest Neighbors (KNN).
+# Classification and Regression Trees (CART).
+# Gaussian Naive Bayes (NB).
+# Support Vector Machines (SVM).
+##############################################################
+# This is a good mixture of simple linear (LR and LDA),
+# nonlinear (KNN, CART, NB and SVM) algorithms.
+##############################################################
+
+algorithms = {}
+results = {}
+
+algorithms['LR'] = LogisticRegression(solver='newton-cg')
+algorithms['LDA'] = LinearDiscriminantAnalysis()
+algorithms['KNN'] = KNeighborsClassifier()
+algorithms['CART'] = DecisionTreeClassifier()
+algorithms['NB'] = GaussianNB()
+algorithms['SVM'] = SVC(gamma='auto')
+
+algorithm_names = list(algorithms.keys())
+
+
+print("Algorithm\t Average\t Standard Deviation")
+print("-"*40)
+
+
+for name in algorithm_names:
+    kfold = model_selection.StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_result = model_selection.cross_val_score(
+        algorithms[name],
+        train_X,
+        train_y,
+        cv=kfold,
+        scoring="accuracy"
+    )
+    results[name] = cv_result
+    print(f"  {name}\t\t   {cv_result.mean():.3f}\t   \u00B1 {cv_result.std():.3f}")
+
+
+df_results = pd.DataFrame(results)
+
+# Algorithm comparison
+# %%
+fig2 = plt.figure(figsize=(6,6))
+
+sns.boxplot(data = df_results)
+
+plt.title("Algorithm Comparison")
+plt.tight_layout()
+plt.show()
 
 
 
-
-
+# %%
